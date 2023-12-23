c571c844a2bad8d592230ed6c970a794cd89db64
Amit Ranjan <amiht.ranjan@gmail.com>
2023-12-23 17:55:02 +0530

    add diff() method in Diff.java

--- Diff.java
+++ Diff.java
@@ -1,6 +1,8 @@
 public class Diff {
-    public static void main(String[] args){
+    public static int diff(int a, int b){
+        return (a-b);
+    }    public static void main(String[] args){
         int a = 20, b = 15;
-        System.out.println(a-b);
+        System.out.println(diff(a,b));
     }
 }
