4e3177d06bf84230e9a5efc129c0a8c8fac28d88
Amit Ranjan <amiht.ranjan@gmail.com>
2023-12-23 18:33:42 +0530

    add multiply method in Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,7 +1,10 @@
 public class Multiply {
-    
+    public static int multiply(int a, int b){
+        return (a*b);
+    }
     public static void main(String[] args){
         int a = 20, b = 6;
-        System.out.println(a*b);
+        System.out.println(multiply(10,6));
     }
+    
 }
