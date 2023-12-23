4c19549c9cc9e8635d00c2fd0cc43698eaa13883
Amit Ranjan <amiht.ranjan@gmail.com>
2023-12-23 17:41:15 +0530

    add sum() method in TestMaster

--- TestMaster.java
+++ TestMaster.java
@@ -1,7 +1,10 @@
 class TestMaster{
+    public static int sum(int a, int b){
+        return (a+b);
+    }
     public static void main(String[] args) {
-        int a = 10, b = 5;
+        int a = 20, b = 51;
         System.out.println("main() : TestMaster");
-        System.out.println(a*b);
+        System.out.println(sum(a, b));
     }
 }
