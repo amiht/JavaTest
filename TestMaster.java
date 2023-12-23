943f28fdc7aa0c9305eb1b262925e5c42110214b
Amit Ranjan <amiht.ranjan@gmail.com>
2023-12-23 16:21:19 +0530

    TestMaster.java

--- TestMaster.java
+++ TestMaster.java
@@ -1,5 +1,7 @@
 class TestMaster{
     public static void main(String[] args) {
+        int a = 10, b = 5;
         System.out.println("main() : TestMaster");
+        System.out.println(a*b);
     }
 }
